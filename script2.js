b7f3702fbacc0d216cb17b740ef9d6ef261cb085
nicholas hung <nhung93@outlook.com>
2020-06-04 23:02:59 -0400

    more progress!:

--- script2.js
+++ script2.js
@@ -203,14 +203,17 @@ function toggleNodeMenu(node){
 class NodeFactory extends AudioContext {
 	// create your new nodes with these functions
 	constructor(){
+		super();
 		this.audioContext = new AudioContext();
 		this.nodeStore = {};  // store refs for nodes
 		this.nodeCounts = {
-			'addWaveNode': function(node){
-				if(this.wavNodeCount){
-					this.waveNodeCount++;
+			// store this function and the node count of diff node types in same object
+			'addNode': function(node){
+				let nodeType = node.constructor.name;
+				if(this[nodeType]){
+					this[nodeType]++;
 				}else{
-					this.waveNodeCount = 1;
+					this[nodeType] = 1;
 				}
 			}
 			
@@ -230,11 +233,50 @@ class NodeFactory extends AudioContext {
 		// clicking on that element will open up a menu to allow the user to change parameters.
 		let osc = this.audioContext.createOscillator();
 		// default params 
-		osc.frequency = 440; // A @ 440 Hz
+		osc.frequency.value = 440; // A @ 440 Hz
 		osc.detune.value = 0;
 		osc.type = "sine";
-		this.nodeCounts.addWaveNode(osc); // increment existing quantity of this node
+		this.nodeCounts.addNode(osc); // increment existing quantity of this node
 		return osc;
+	}
+	
+	_createNoiseNode(){
+	
+		// allow user to pass in the contents of the noise buffer as a list if they want to?
+		let noise = audioContext.createBufferSource();
+		
+		// assign random noise first, but let it be customizable
+		let bufSize = audioContext.sampleRate; // customizable?
+		let buffer = audioContext.createBuffer(1, bufSize, bufSize);
+		
+		let output = buffer.getChannelData(0);
+		for(let i = 0; i < bufSize; i++){
+			output[i] = Math.random() * 2 - 1;
+		}
+		
+		noise.buffer = buffer;
+		
+		this.nodeCounts.addNode(noise);
+		return noise;
+		
+		// note that the filter has its own gain value.
+		// increase filter gain == stronger filter
+		//let noiseFilter = createBiquadFilterNode(audioContext);
+		//noise.connect(noiseFilter);
+
+		// add gain (for volume) to the noise filter 
+		//let noiseVolume = audioContext.createGain();
+		//noiseFilter.connect(noiseVolume);
+		//noiseVolume.connect(audioContext.destination);
+		//return [noise, noiseVolume];	
+	}
+	
+	_createGainNode(){
+		let gainNode = this.audioContext.createGain();
+		this.nodeCounts.addNode(gainNode);
+		// gain will alwaays need to attach to context destination
+		gainNode.connect(this.audioContext.destination);
+		return gainNode;
 	}
 	
 	// create a biquadfilter node
@@ -247,16 +289,43 @@ class NodeFactory extends AudioContext {
 		bqFilterNode.type = "lowpass";
 		
 		// need to add to nodeCounts
-		
+		this.nodeCounts.addNode(bqFilterNode);
 		return bqFilterNode;
 	}
 	
-	_addNodeToInterface(nodeUIElement){
+	_addNodeToInterface(node, x, y){
 		// place randomly in designated area?
+		let uiElement = this._createNodeUIElement(node);
+		
+		uiElement.style.top = x || '300px';
+		uiElement.style.left = y || '300px';
+		
+		document.body.appendChild(uiElement);
 	}
 	
 	_createNodeUIElement(node){
 		// add event listener to allow it to be hooked up to another node if possible
+		let customizableProperties = Object.keys(node.__proto__);
+		let uiElement = document.createElement('div');
+		//uiElement.style.width = '300px';
+		//uiElement.style.height = '210px';
+		uiElement.style.position = 'absolute';
+		uiElement.style.border = '1px solid #000';
+		uiElement.style.borderRadius = '20px 20px 20px 20px';
+		uiElement.style.textAlign = 'center';
+		
+		let name = document.createElement('h4');
+		name.textContent = node.constructor.name;
+		uiElement.appendChild(name);
+	
+		customizableProperties.forEach((prop) => {
+			let property = document.createElement('p');
+			property.textContent = prop;
+			
+			uiElement.appendChild(property);
+		});
+		
+		return uiElement;
 	}
 	
 	// create and add a new wave node to the interface 
@@ -267,17 +336,26 @@ class NodeFactory extends AudioContext {
 		let newNode = null;
 		
 		if(nodeType === "waveNode"){
-			newNode = _createWaveNode();
+			newNode = this._createWaveNode();
 		}else if(nodeType === "biquadFilterNode"){
+			newNode = this._createBiquadFilterNode();
+		}else if(nodeType === "noiseNode"){
+			newNode = this._createNoiseNode();
+		}else if(nodeType === "gainNode"){
+			newNode = this._createGainNode();
+		}else{
+			console.log("unknown node type!");
+			return;
 		}
 		
 		// store it 
-		_storeNode(newNode, (nodeType + this.nodeCounts[(nodeType + "Count")]));
+		this._storeNode(newNode, (nodeType + this.nodeCounts[(nodeType + "Count")]));
 		
 		// this should be a separate function
 		if(addToInterface){
 			// add it visually to the UI 
 			// add event listeners to the UI
+			this._addNodeToInterface(newNode);
 		}
 	}
 	
@@ -285,280 +363,28 @@ class NodeFactory extends AudioContext {
 
 // maybe move all the UI handling stuff to another class that will be comprised of the nodefactory class?
 class SoundMaker {
-
+	
 	constructor(){
 		this.nodeFactory = new NodeFactory();
 	}
-
-
+	
 }
 
 
-
-function setElementAttributes(element, dict){
-	for(let attr in dict){
-		element.setAttribute(attr, dict[attr]);
-	}
-}
-
-
-
-function createNewWavOsc(parentElement){
-	// create a new wave osc section in html 
-	let newNodeParams = {};
-	
-	let newIdNum = currPreset.numWaveNodes + 1;
-	let newWaveNodeDiv = document.createElement('div');
-	newWaveNodeDiv.id = 'waveNode' + newIdNum;
-	newWaveNodeDiv.className = 'waveNode';
-	newWaveNodeDiv.style.border = "1px solid #000";
-	newWaveNodeDiv.style.marginBottom = "5px";
-	newWaveNodeDiv.style.padding = "5px";
-	
-	let h3 = document.createElement('h3');
-	h3.innerHTML = 'wave node ' + newIdNum;
-	
-	// input for volume
-	let input = document.createElement('input');
-	setElementAttributes(
-		input,
-		{
-			'name': ('waveOscVolume' + newIdNum),
-			'id': ('waveOscVolume' + newIdNum),
-			'type': 'range',
-			'max': 0.5,
-			'min': 0.0,
-			'step': 0.01,
-			'value': 0.08
-		}
-	);
-	
-	let volumeValue = document.createElement('label');
-	volumeValue.id = 'waveNodeVolValue' + newIdNum;
-	volumeValue.innerHTML = input.value;
-	
-	let label = document.createElement('label');
-	label.innerHTML = 'wav osc vol: ';
-	label.setAttribute('for', 'waveOscVolume' + newIdNum);
-	
-	// input for detune
-	let detuneInput = document.createElement('input');
-	detuneInput.setAttribute('name', 'waveOscDetune' + newIdNum);
-	detuneInput.setAttribute('id', 'waveOscDetune' + newIdNum);
-	detuneInput.setAttribute('type', 'range');
-	detuneInput.setAttribute('max', 100);
-	detuneInput.setAttribute('min', -100);
-	detuneInput.setAttribute('step', 1);
-	detuneInput.setAttribute('value', 0);
-	
-	let detuneValue = document.createElement('label');
-	detuneValue.id = 'waveNodeDetuneValue' + newIdNum;
-	detuneValue.innerHTML = detuneInput.value;
-	
-	let detuneLabel = document.createElement('label');
-	detuneLabel.innerHTML = 'wav osc detune: ';
-	detuneLabel.setAttribute('for', 'waveOscDetune' + newIdNum);
-	
-	let brElement1 = document.createElement('br');
-	let brElement2 = document.createElement('br');
-	let brElement3 = document.createElement('br');
-	
-	// select for oscillator wave type 
-	let select = document.createElement('select');
-	select.setAttribute('name', 'waveOscType' + newIdNum);
-	select.id = 'waveOscType' + newIdNum;
-	
-	let options = [
-		'square',
-		'triangle',
-		'sawtooth'
-	];
-	
-	options.forEach((opt) => {
-		let newOption = document.createElement('option');
-		newOption.innerHTML = opt;
-		select.appendChild(newOption);
-	});
-	
-	let selectLabel = document.createElement('label');
-	selectLabel.innerHTML = 'wav osc type: ';
-	selectLabel.setAttribute('for', 'waveOscType' + newIdNum);
-	
-	// add event listeners for input and select
-	input.addEventListener('input', (evt) => {
-		newNodeParams[getAlphaString(input.id)] = evt.target.valueAsNumber;
-		volumeValue.innerHTML = evt.target.valueAsNumber;
-	});
-	
-	detuneInput.addEventListener('input', (evt) => {
-		newNodeParams[getAlphaString(detuneInput.id)] = evt.target.valueAsNumber;
-		detuneValue.innerHTML = evt.target.valueAsNumber;		
-	});
-
-	select.addEventListener('change', (evt) => {
-		newNodeParams[getAlphaString(select.id)] = evt.target.value;
-	});
-	
-	// put it all together
-	newWaveNodeDiv.appendChild(h3);
-	
-	newWaveNodeDiv.appendChild(label);
-	newWaveNodeDiv.appendChild(input);
-	newWaveNodeDiv.appendChild(volumeValue);
-	newWaveNodeDiv.appendChild(brElement2);
-	
-	newWaveNodeDiv.appendChild(detuneLabel);
-	newWaveNodeDiv.appendChild(detuneInput);
-	newWaveNodeDiv.appendChild(detuneValue);
-	newWaveNodeDiv.appendChild(brElement3);
-	
-	newWaveNodeDiv.appendChild(brElement1);
-	newWaveNodeDiv.appendChild(selectLabel);
-	newWaveNodeDiv.appendChild(select);
-	
-	// TODO: don't need this after dynamic noise node creation implemented?
-	let parent = document.getElementById(parentElement);
-	parent.insertBefore(newWaveNodeDiv, parent.firstChild);
-	
-	// add all attributes of node to object
-	newNodeParams[getAlphaString(input.id)] = 0.08;
-	newNodeParams[getAlphaString(detuneInput.id)] = 0;
-	newNodeParams[getAlphaString(select.id)] = 'square';
-	
-	currPreset.numWaveNodes += 1;
-	currPreset.waveNodes.push(newNodeParams);
-}
-
-function createNewNoiseOsc(parentElement){
-
-	// create a new noise section in html 
-	let newNodeParams = {};
-	
-	let newIdNum = currPreset.numNoiseNodes + 1;
-	let newNoiseNodeDiv = document.createElement('div');
-	newNoiseNodeDiv.id = 'noiseNode' + newIdNum;
-	newNoiseNodeDiv.className = 'noiseNode';
-	newNoiseNodeDiv.style.border = "1px solid #000";
-	newNoiseNodeDiv.style.marginBottom = "5px";
-	newNoiseNodeDiv.style.padding = "5px";
-	
-	let h3 = document.createElement('h3');
-	h3.innerHTML = 'noise node ' + newIdNum;
-	
-	let input = document.createElement('input');
-	input.setAttribute('name', 'noiseOscVolume' + newIdNum);
-	input.setAttribute('id', 'noiseOscVolume' + newIdNum);
-	input.setAttribute('type', 'range');
-	input.setAttribute('max', 0.2);
-	input.setAttribute('min', 0.0);
-	input.setAttribute('step', 0.01);
-	input.setAttribute('value', 0.1);
-	
-	let volumeValue = document.createElement('label');
-	volumeValue.id = 'noiseNodeVolValue' + newIdNum;
-	volumeValue.innerHTML = input.value;
-	
-	let label = document.createElement('label');
-	label.innerHTML = 'noise osc vol: ';
-	label.setAttribute('for', 'noiseOscVolume' + newIdNum);
-	
-	let brElement1 = document.createElement('br');
-	let brElement2 = document.createElement('br');
-	let brElement3 = document.createElement('br');
-	
-	let freqInput = document.createElement('input');
-	freqInput.setAttribute('name', 'noiseOscFreq' + newIdNum);
-	freqInput.setAttribute('id', 'noiseOscFreq' + newIdNum);
-	freqInput.setAttribute('type', 'range');
-	freqInput.setAttribute('max', 5000);
-	freqInput.setAttribute('min', 500);
-	freqInput.setAttribute('step', 100);
-	freqInput.setAttribute('value', 1800);
-	
-	let freqInputLabel = document.createElement('label');
-	freqInputLabel.innerHTML = 'noise osc freq: ';
-	freqInputLabel.setAttribute('for', 'noiseOscFreq' + newIdNum);
-	
-	let freqValue = document.createElement('label');
-	freqValue.id = 'noiseOscFreqValue' + newIdNum;
-	freqValue.innerHTML = freqInput.value;
-	
-	let select = document.createElement('select');
-	select.setAttribute('name', 'noiseFilterPassType' + newIdNum);
-	select.id = 'noiseFilterPassType' + newIdNum;
-	
-	let filterOptions = [
-		'highpass',
-		'lowpass'
-	];
-	
-	filterOptions.forEach((opt) => {
-		let newOption = document.createElement('option');
-		newOption.innerHTML = opt;
-		select.appendChild(newOption);
-	});
-	
-	let selectLabel = document.createElement('label');
-	selectLabel.innerHTML = 'noise filter pass type: ';
-	selectLabel.setAttribute('for', 'noiseFilterPassType' + newIdNum);
-	
-	// add event listeners for input and select
-	input.addEventListener('input', (evt) => {
-		newNodeParams[getAlphaString(input.id)] = evt.target.valueAsNumber;
-		volumeValue.innerHTML = evt.target.valueAsNumber;
-	});
-	
-	freqInput.addEventListener('input', (evt) => {
-		newNodeParams[getAlphaString(freqInput.id)] = evt.target.valueAsNumber;
-		freqValue.innerHTML = evt.target.valueAsNumber;
-	});
-
-	select.addEventListener('change', (evt) => {
-		newNodeParams[getAlphaString(select.id)] = evt.target.value;
-	});
-	
-	// put it all together
-	newNoiseNodeDiv.appendChild(h3);
-	newNoiseNodeDiv.appendChild(label);
-	newNoiseNodeDiv.appendChild(input);
-	newNoiseNodeDiv.appendChild(volumeValue);
-	newNoiseNodeDiv.appendChild(brElement1);
-	newNoiseNodeDiv.appendChild(freqInputLabel);
-	newNoiseNodeDiv.appendChild(freqInput);
-	newNoiseNodeDiv.appendChild(freqValue);
-	newNoiseNodeDiv.appendChild(brElement2);
-	newNoiseNodeDiv.appendChild(selectLabel);
-	newNoiseNodeDiv.appendChild(select);
-	newNoiseNodeDiv.appendChild(brElement3);
-	
-	// TODO: don't need this after dynamic noise node creation implemented?
-	let parent = document.getElementById(parentElement);
-	parent.insertBefore(newNoiseNodeDiv, parent.firstChild);
-	
-	newNodeParams[getAlphaString(input.id)] = 0.1;
-	newNodeParams[getAlphaString(select.id)] = 'highpass';
-	newNodeParams[getAlphaString(freqInput.id)] = 1800;
-	
-	currPreset.numNoiseNodes += 1;
-	currPreset.noiseNodes.push(newNodeParams);
-}
-
-// create initial nodes
-createNewWavOsc('instrumentPreset');
-createNewNoiseOsc('instrumentPreset');
-
-document.getElementById('instrumentPresetDownload').addEventListener('click', () => {
-	downloadPreset();
+let soundMaker = new SoundMaker();
+document.getElementById('addWavNode').addEventListener('click', (e) => {
+	soundMaker.nodeFactory.addNewNode("waveNode");
 });
 
-document.getElementById('addNewWavNode').addEventListener('click', () => {
-	createNewWavOsc('instrumentPreset');
+document.getElementById('addGainNode').addEventListener('click', (e) => {
+	soundMaker.nodeFactory.addNewNode("gainNode");
 });
 
-document.getElementById('addNewNoiseNode').addEventListener('click', () => {
-	createNewNoiseOsc('instrumentPreset');
+document.getElementById('addNoiseNode').addEventListener('click', (e) => {
+	soundMaker.nodeFactory.addNewNode("noiseNode");
 });
 
-document.getElementById('instrumentPresetImport').addEventListener('click', function(){
-	importInstrumentPreset();
+document.getElementById('addFilterNode').addEventListener('click', (e) => {
+	soundMaker.nodeFactory.addNewNode("biquadFilterNode");
 });
+
